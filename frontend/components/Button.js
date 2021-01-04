ff58077dc91d0053b518ea4b8ba125aea716f984
Ekunola John <tobiz2002@gmail.com>
2021-01-04 23:22:50 +0100

    Fix(next router link):  ref forwarding error on the console

--- frontend/components/Button.js
+++ frontend/components/Button.js
@@ -1,21 +1,22 @@
+import React from "react";
 import Link from "next/link";
 import StyledButton from "./StyledButton";
 import StyledLinkButton from "./StyledLinkButton";
 // import PropTypes from 'prop-types'
 
-function RenderButton(props) {
+const RenderButton = React.forwardRef((props, ref) => {
   const { link, icon, children, ...otherProps } = props;
   let ButtonComponent = StyledButton;
 
   if (link) ButtonComponent = StyledLinkButton;
 
   return (
-    <ButtonComponent icon={!!icon} as={link && "a"} {...otherProps}>
-      {typeof children === "string" ? <span>{children}</span> : { children }}
+    <ButtonComponent ref={ref} icon={!!icon} as={link && "a"} {...otherProps}>
+      {typeof children === "string" ? <span>{children}</span> : children}
       {icon && <span className="btn-icon">{icon}</span>}
     </ButtonComponent>
   );
-}
+});
 
 function Button(props) {
   const { link, href } = props;
